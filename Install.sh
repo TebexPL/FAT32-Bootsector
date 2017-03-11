d33660160499c2115778f2fbf77d572d6d0c7869
TebexPL <kipacz123@gmail.com>
2017-03-11 17:33:41 +0100

    Install script is almost perfect(almost every case is covered)

--- Install.sh
+++ Install.sh
@@ -1,6 +1,29 @@
+RED='\033[0;31m'
+NC='\033[0m' 
 clear
+printf "${RED}"
+echo "This script needs NASM to compile source code, It will be installed right now"
+printf "${NC}"
+echo "(press anything to continue)"
+read -n 1 -s
+clear
+sudo apt-get install nasm -qq > /dev/null
+
+
+
+
+bool="1"
+
+clear
+printf "${RED}"
 echo "To exit press enter with no value, at any time"
+printf "${NC}"
+echo "(press anything to continue)"
+read -n 1 -s
+while [ "$bool" = "1" ]; do
+clear
 echo "Filename without extention: "
+echo "---------------------------"
 read 
 if [ -z "$REPLY" ] ; then
 	exit 
@@ -14,8 +37,9 @@ filename=$REPLY
 while (( "${#_FILENAME}" < "8" )) ; do
 	_FILENAME="$_FILENAME "
 done
-
+clear
 echo "Extention: "
+echo "----------"
 read
 if [ -z "$REPLY" ] ; then
 	exit 
@@ -33,12 +57,35 @@ done
 
 _FILENAME=$(echo "$_FILENAME" | awk '{printf toupper($0)}')
 
+clear
+echo "Is that correct?(y/n)"
+echo "---------------------"
+echo "Filename: "$filename
+
+read
+if [ -z "$REPLY" ] ; then
+		exit 
+fi
+if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ] ; then
+	bool="0"
+else
+	bool="1"
+fi
+
+done
 
 nasm -d _FILENAME=\'"$_FILENAME"\' -o bin/bootloader.bin -f bin src/bootloader.asm
 
 clear
-echo "CHECK TWICE WHAT YOU'RE DOING NOW, YOUR PC MAY NOT BOOT IF YOU SCREW UP SOMETHING";
-echo "Choose device to install bootsector:"
+
+printf "${RED}"
+echo "CHECK TWICE WHAT YOU'RE DOING NOW, IF YOU CHOOSE WRONG DEVICE YOUR PC WON'T BOOT";
+printf "${NC}"
+echo "(press anything to continue)"
+read -n 1 -s 
+clear
+echo "Choose device you want to boot from:"
+echo "--------------------------------------------------------------------------------"
 devices=$(sudo ls /dev/disk/by-id/ | grep -v "part[0-9]$");
 num=1
 for word in $devices
@@ -62,10 +109,14 @@ done
 
 bootdev=${device[$REPLY]}
 clear
-echo "Choose partition be set as boot:"
-parts=$(sudo parted /dev/disk/by-id/${device[$REPLY]} print | grep "^ [0-9]");
+echo "Choose partition you want to boot from:"
+echo "---------------------------------------"
+parts="$(sudo parted /dev/disk/by-id/${device[$REPLY]} print | grep "^ [0-9]")";
+
 if [ $? -eq 0 ]; then
-    echo 
+	info=$(sudo parted /dev/disk/by-id/${device[$REPLY]} print | grep "^Number");
+	info=${info%"Flags"}
+    echo "$info"
 else
 	clear
 	echo "Bad data or device not available"
@@ -76,7 +127,8 @@ num=1
 while read -r line; do
 
    	part[$num]=$line
-   	echo ${part[$num]};
+	curpart=${part[$num]%"boot"}
+    echo " $curpart"
  	num=$((num+1));
 done <<< "$parts"
 read
@@ -91,9 +143,11 @@ done
 clear
 bootpart=$REPLY
 echo "Are you sure you want to install bootsector on this device?[y/n]"
-echo
-echo "Device: "$bootdev
-echo "Bootable Partition: "${part[$REPLY]}
+echo "----------------------------------------------------------------"
+echo "Device:    "$bootdev
+echo 
+echo "Partition:  $info"
+echo "            ${part[$REPLY]%"boot"}"
 read
 if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ] ; then
 	echo 
@@ -104,15 +158,22 @@ fi
 	tmp=$(wc -c ./bin/bootloader.bin | awk '{printf $1}')
 	if (("$tmp" > "446")) ; then
 		clear
+		printf "${RED}"
 		echo "Critical Error - bootsector code exceeds 446 bytes";
+		echo "No changes were made to your device";
+		printf "${NC}"
 		exit
 	fi
 	if [ -z "$(echo ${part[$bootpart]} | grep "boot" )" ] ; then
 		sudo parted /dev/disk/by-id/$bootdev toggle $bootpart boot > /dev/null
 	fi
-	sudo dd if=./bin/bootloader.bin of=/dev/disk/by-id/$bootdev
 	clear
-	echo "DONE"
-	echo "Now copy "$filename" to root directory of selected partition, and you can boot from this device"
-
+	sudo dd if=./bin/bootloader.bin of=/dev/disk/by-id/$bootdev status=none
+	echo "DONE!" 
+	printf "${RED}\n"
+	printf "Now copy "${NC}$filename${RED}" to root directory of selected partition, and you're good to go"
+	printf "${NC}\n"
+	echo "(press anything to end)"
+	read -n 1 -s
+	clear
 
